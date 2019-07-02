0c99b298b9c9d0b1e90431eaeffabcbd4196fbbe
ViJaY aHiRe <evijay@users.noreply.github.com>
2019-07-02 21:00:28 +0530

    Marathi Language file
    Marathi Language file

---
+++ src/locale/lang/mr.js
@@ -0,0 +1,109 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'ठीक',
+      clear: 'साफ करा'
+    },
+    datepicker: {
+      now: 'आता',
+      today: 'आज',
+      cancel:  'रद्द करा',
+      clear: 'साफ करा',
+      confirm: 'ठीक',
+      selectDate: 'तारीख निवडा',
+      selectTime: 'वेळ निवडा',
+      startDate: 'प्रारंभ तारीख',
+      startTime: 'प्रारंभीची वेळ',
+      endDate: 'समाप्तीची तारीख',
+      endTime: 'समाप्तीची वेळ',
+      prevYear: 'मागील वर्ष',
+      nextYear: 'पुढील वर्ष',
+      prevMonth: 'मागील महिना',
+      nextMonth: 'पुढील महिना',
+      year: '',
+      month1: 'जानेवारी',
+      month2: 'फेब्रुवारी',
+      month3: 'मार्च',
+      month4: 'एप्रिल',
+      month5: 'मे',
+      month6: 'जून',
+      month7: 'जुलै',
+      month8: 'ऑगस्ट',
+      month9: 'सप्टेंबर',
+      month10: 'ऑक्टोबर',
+      month11: 'नोव्हेंबर',
+      month12: 'डिसेंबर',
+      week: 'आठवडा',
+      weeks: {
+        sun: 'रवि',
+        mon: 'सोम',
+        tue: 'मंगळ',
+        wed: 'बुध',
+        thu: 'गुरु',
+        fri: 'शुक्र',
+        sat: 'शनि'
+      },
+      months: {
+        jan: 'जाने',
+        feb: 'फेब्रु',
+        mar: 'मार्च',
+        apr: 'एप्रि',
+        may: 'मे',
+        jun: 'जून',
+        jul: 'जुलै',
+        aug: 'ऑग',
+        sep: 'सप्टें',
+        oct: 'ऑक्टो',
+        nov: 'नोव्हें',
+        dec: 'डिसें'
+      }
+    },
+    select: {
+      loading: 'लादणे चालू आहे',
+      noMatch: 'कोणताही जुळणारी माहिती उपलब्ध नाही',
+      noData: 'माहिती उपलब्ध नाही',
+      placeholder: 'निवडा'
+    },
+    cascader: {
+      noMatch: 'कोणताही जुळणारी माहिती उपलब्ध नाही',
+      loading: 'लादणे चालू आहे',
+      placeholder: 'निवडा'
+    },
+    pagination: {
+      goto: 'जा',
+      pagesize: '/पृष्ठ',
+      total: 'एकूण {एकूण}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'संदेश',
+      confirm: 'ठीक',
+      cancel: 'रद्द करा',
+      error: 'अवैध इनपुट'
+    },
+    upload: {
+      deleteTip: 'काढण्यासाठी डिलीट दाबा',
+      delete: 'डिलीट',
+      preview: 'पूर्वावलोकन',
+      continue: 'सुरू ठेवा'
+    },
+    table: {
+      emptyText: 'माहिती उपलब्ध नाही',
+      confirmFilter: 'खात्रीपूर्वक',
+      resetFilter: 'पुन्हा रचना करा',
+      clearFilter: 'सर्व',,
+      sumText: 'Sum'
+    },
+    tree: {
+      emptyText: 'माहिती उपलब्ध नाही'
+    },
+    transfer: {
+      noMatch: 'कोणताही जुळणारी माहिती उपलब्ध नाही',
+      noData: 'माहिती उपलब्ध नाही',
+      titles: ['सूची 1', 'यादी 2'], // to be translated
+      filterPlaceholder: 'कीवर्ड प्रविष्ट करा', // to be translated
+      noCheckedFormat: '{एकूण} आयटम्स', // to be translated
+      hasCheckedFormat: '{तपासले}/{एकूण} तपासले' // to be translated
+    }
+  }
+};
