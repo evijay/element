938f8e26cd261dd8ecf26fcae8081677019f2acc
ViJaY aHiRe <evijay@users.noreply.github.com>
2019-07-03 14:51:37 +0530

    Hindi Language file
    Hindi Language file

---
+++ src/locale/lang/hi.js
@@ -0,0 +1,109 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'ठीक है',
+      clear: 'साफ़'
+    },
+    datepicker: {
+      now: 'अभी',
+      today: 'आज',
+      cancel:  'रद्द करें',
+      clear: 'साफ़',
+      confirm: 'ठीक है',
+      selectDate: 'तारीख़ चुनें',
+      selectTime: 'समय का चयन करें',
+      startDate: 'प्रारंभ तारीख',
+      startTime: 'समय शुरू',
+      endDate: 'अंतिम तिथि',
+      endTime: 'अंतिम समय',
+      prevYear: 'पिछला साल',
+      nextYear: 'अगले वर्ष',
+      prevMonth: 'पिछ्ला महिना',
+      nextMonth: 'अगले महिना',
+      year: '',
+      month1: 'जनवारी',
+      month2: 'फरवरी',
+      month3: 'मार्च',
+      month4: 'अप्रैल',
+      month5: 'मई',
+      month6: 'जून',
+      month7: 'जुलाई',
+      month8: 'अगस्त',
+      month9: 'सितंबर',
+      month10: 'अक्टूबर',
+      month11: 'नवंबर',
+      month12: 'दिसंबर',
+      week: 'सप्ताह',
+      weeks: {
+        sun: 'रवि',
+        mon: 'सोम',
+        tue: 'मंगल',
+        wed: 'बुध',
+        thu: 'गुरु',
+        fri: 'शुक्र',
+        sat: 'शनि'
+      },
+      months: {
+        jan: 'जनवरी',
+        feb: 'फरवरी',
+        mar: 'मार्च',
+        apr: 'अप्रैल',
+        may: 'मई',
+        jun: 'जून',
+        jul: 'जुलाई',
+        aug: 'अगस्त',
+        sep: 'सितंबर',
+        oct: 'अक्टूबर',
+        nov: 'नवंबर',
+        dec: 'दिसंबर'
+      }
+    },
+    select: {
+      loading: 'लोड हो रहा है',
+      noMatch: 'कोई मेल नहीं मिला',
+      noData: 'कोई आकड़ा उपलब्ध नहीं है',
+      placeholder: 'चुने'
+    },
+    cascader: {
+      noMatch: 'कोई मेल नहीं मिला',
+      loading: 'लोड हो रहा है',
+      placeholder: 'चुने'
+    },
+    pagination: {
+      goto: 'जाओ',
+      pagesize: '/पृष्ठ',
+      total: 'कुल {कुल}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'संदेश',
+      confirm: 'ठीक',
+      cancel: 'रद्द करें',
+      error: 'अवैध इनपुट'
+    },
+    upload: {
+      deleteTip: 'हटाने के लिए डिलीट दबाएं',
+      delete: 'हटाना',
+      preview: 'पूर्वावलोकन',
+      continue: 'जारी रहना'
+    },
+    table: {
+      emptyText: 'कोई आकड़ा उपलब्ध नहीं है',
+      confirmFilter: 'खात्रीपूर्वक',
+      resetFilter: 'स्थापित करे',
+      clearFilter: 'सब',
+      sumText: 'योग पाठ'
+    },
+    tree: {
+      emptyText: 'कोई आकड़ा उपलब्ध नहीं है'
+    },
+    transfer: {
+      noMatch: 'कोई मेल नहीं मिला',
+      noData: 'माहिती उपलब्ध नाही',
+      titles: ['सूची 1', 'सूची 2'], // to be translated
+      filterPlaceholder: 'संकेत शब्द दर्ज करें', // to be translated
+      noCheckedFormat: '{एकूण} चीजे', // to be translated
+      hasCheckedFormat: '{जांचा हुआ}/{एकूण} जांचा हुआ' // to be translated
+    }
+  }
+};
